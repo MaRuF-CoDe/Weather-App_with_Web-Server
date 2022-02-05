d0033fa4e03bf575cd1489472dfadc6f2a2f426f
Maruf <alenmaruf.am@gmail.com>
2022-02-06 04:03:22 +0600

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Maruf'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Maruf Bangabashi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
